b8eb4b8f77ea65fe7ef662fe7561a00e09470ffc
Dawid Pachciarek <dawid.pachciarek@usertive.com>
2022-11-24 12:06:04 +0100

    fix: resolvePathName and LangRouter should should not cut off parameters

--- lib/src/client/LangRouter.tsx
+++ lib/src/client/LangRouter.tsx
@@ -1,18 +1,13 @@
 import {NextRouter, useRouter} from 'next/router';
 import {memo} from 'react';
 import {useTranslation} from 'react-i18next';
-import {join} from 'path';
-import {resolvePathName} from '../utils';
 
 function LangRouter() {
   const router: NextRouter = useRouter();
   const {i18n} = useTranslation();
 
   if (i18n.language) {
-    const interpolatedRoute: string = resolvePathName(router.pathname, router.query);
-    const resolvedPath: string = join('/' + i18n.language, interpolatedRoute).replace(/\/$/, '');
-
-    void router.push(resolvedPath);
+    void router.push({pathname: `/[lang]${router.pathname}`, query: {...router.query, lang: i18n.language}});
   }
 
   return null;
--- lib/src/utils/resolvePathName.ts
+++ lib/src/utils/resolvePathName.ts
@@ -1,10 +1,19 @@
 import {ParsedUrlQuery} from 'querystring';
 
 export function resolvePathName(pathName: string, queryParams: ParsedUrlQuery) {
+  let amountOfParameters = 0;
   for (const queryParam in queryParams) {
     const queryParamValue: string = Array.isArray(queryParams[queryParam])
       ? (queryParams[queryParam] as string[]).join('')
       : (queryParams[queryParam] as string);
+    const newPathName = pathName.replaceAll(`[${queryParam}]`, queryParamValue);
+    if(newPathName !== pathName) {
+        pathName = newPathName;
+    } else {
+        const char = amountOfParameters > 0 ? "&" : "?";
+        pathName = `${pathName}${char}${queryParam}=${queryParamValue}`
+        amountOfParameters++;
+    }
 
     pathName = pathName.replaceAll(`[${queryParam}]`, queryParamValue);
   }
