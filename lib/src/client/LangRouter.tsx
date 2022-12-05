fb1ffc52968ea0b7c2c66017c12e8aa485cbb080
Dawid Pachciarek <dawid.pachciarek@usertive.com>
2022-12-05 16:06:07 +0100

    fix: resolve abort fetching component error

--- lib/src/client/LangRouter.tsx
+++ lib/src/client/LangRouter.tsx
@@ -1,14 +1,15 @@
 import {NextRouter, useRouter} from 'next/router';
-import {memo} from 'react';
+import {memo, useEffect} from 'react';
 import {useTranslation} from 'react-i18next';
 
 function LangRouter() {
   const router: NextRouter = useRouter();
   const {i18n} = useTranslation();
-
-  if (i18n.language) {
-    void router.push({pathname: `/[lang]${router.pathname}`, query: {...router.query, lang: i18n.language}});
-  }
+  useEffect(()=>{
+    if (i18n.language && router.isReady) {
+      void router.push({pathname: `/[lang]${router.pathname}`, query: {...router.query, lang: i18n.language}});
+    }
+  },[i18n.language, router.isReady])
 
   return null;
 }
